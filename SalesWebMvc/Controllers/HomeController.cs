6b007a0a886b48793045448076d10150e263586c
Daniel Rodrigues <dantdmc210@gmail.com>
2020-07-24 10:17:54 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles web mvc app from c# course";
+            ViewData["email"] = "danielsrdev98@gmail.com";
 
             return View();
         }
